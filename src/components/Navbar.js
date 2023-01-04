3dc677fe388fd2e49773d164fbb51232f75b2f35
ankitchaudharyy <ankitdelhi20@gmail.com>
2023-01-04 18:18:28 +0530

    [Ankit|Procheta|Roshni] Add. navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+import "./Navbar.css";
+
+const RedirectToThoughtworksPage = () => {
+    window.open("https://www.thoughtworks.com/");
+}
+
+export const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ bgcolor: "#edf1f3", boxShadow: "none", color: "black" }}>
+        <Toolbar className="navbar">
+          <Typography className="navbar_button" variant="h7" component="div" onClick={RedirectToThoughtworksPage}>
+            Thoughtworks
+          </Typography>
+            <div className='buttons'>
+                <div className="navbar_button" color="inherit">About</div>
+                <div className="navbar_button" color="inherit">Agenda</div>
+                <div className="navbar_button" color="inherit">Speakers</div>
+                <div className="navbar_button" color="inherit">Registration</div>
+            </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+
