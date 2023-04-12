cae0d8bb17a9074d76f71b568e113c0c02ff4c33
ankitchaudharyy <ankitdelhi20@gmail.com>
2023-04-12 19:30:56 +0530

    [Ankit | Procheta] Update. Agenda details

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -8,25 +8,156 @@ export const Agenda = () => {
       <h2 className="agenda_heading">Agenda</h2>
       <div className="agenda_schedule">
         <div className="agenda_day">
-          <p className="agenda_subheading">Day 1 - 25th March, 2022</p>
+          <p className="agenda_subheading">Gurgaon</p>
         </div>
         <div className="schedule-info">
-          <div className="timing">14:00 - 14:10</div>
+          <div className="timing">10:00 - 10:45</div>
           <div className="seperation">$</div>
-          <div className="detail">Introduction to Converge.</div>
+          <div className="detail">Keynote</div>
         </div>
         <div className="schedule-info">
-          <div className="timing">14:00 - 14:10</div>
+          <div className="timing">10:45 - 11:00</div>
           <div className="seperation">$</div>
-          <div className="detail">Metaverse as a Service Vinodh Arumugam</div>
+          <div className="detail">Agenda & Ice Breaker</div>
         </div>
         <div className="schedule-info">
-          <div className="timing">14:00 - 14:10</div>
+          <div className="timing">11:00 - 11:45</div>
+          <div className="seperation">$</div>
+          <div className="detail">Bootstrapping Datamesh for a complex enterprise (TDK)</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">11:45 - 12:00</div>
+          <div className="seperation">$</div>
+          <div className="detail">Break</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">12:00 - 12:30</div>
           <div className="seperation">$</div>
           <div className="detail">
-            XaaS: Looking back to look forward Panelists - Madhavi Das, Rohit
-            Karlo, Gunjan Shukla Moderator - Vinod Sankaranarayanan
+          Continous Delivery for data pipelines
           </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">12:30 - 13:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">From data to diversity: a Data Strategy case study on Thoughtworks</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">13:15 - 14:00</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Lunch </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">14:00 - 14:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Game</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">14:15 - 15:00</div>
+          <div className="seperation">$</div>
+          <div className="detail">Dynamic pricing for hotel Industry</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">15:00 - 15:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Break</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">15:15 - 16:00</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Reproducible ML pipelines with Vertex AI, Github actions, Hydra and DVC </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">16:00 - 16:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Break</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">16:15 - 17:15</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Hands on workshop </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">17:15 - 18:00</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Recruitment</div>
+        </div>
+      </div>
+      <div className="agenda_schedule">
+        <div className="agenda_day">
+          <p className="agenda_subheading">Bangalore</p>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">10:00 - 10:45</div>
+          <div className="seperation">$</div>
+          <div className="detail">Keynote</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">10:45 - 11:00</div>
+          <div className="seperation">$</div>
+          <div className="detail">Agenda & Ice Breaker</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">11:00 - 11:45</div>
+          <div className="seperation">$</div>
+          <div className="detail">Data Mesh : Streamlined Developer Experience in data mesh </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">11:45 - 12:00</div>
+          <div className="seperation">$</div>
+          <div className="detail">Break</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">12:00 - 12:30</div>
+          <div className="seperation">$</div>
+          <div className="detail">
+          Designing Fast Data Applications	
+          </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">12:30 - 13:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Quality in the World of Data</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">13:15 - 14:00</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Lunch </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">14:00 - 14:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Game</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">14:15 - 15:00</div>
+          <div className="seperation">$</div>
+          <div className="detail">Domain based Big Data Loop for Autonomous Driving	</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">15:00 - 15:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Break</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">15:15 - 16:00</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Jugalbandi: GPT3-powered Indic Chatbot		 </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">16:00 - 16:15</div>
+          <div className="seperation">$</div>
+          <div className="detail">Break</div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">16:15 - 17:15</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Hands on workshop </div>
+        </div>
+        <div className="schedule-info">
+          <div className="timing">17:15 - 18:00</div>
+          <div className="seperation">$</div>
+          <div className="detail"> Recruitment</div>
         </div>
       </div>
     </div>
