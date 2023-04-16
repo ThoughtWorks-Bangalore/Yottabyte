3d1958f17d0b6f9d1af756fd41bdfac80a97c8bc
ankitchaudharyy <ankitdelhi20@gmail.com>
2023-04-16 22:32:34 +0530

    [Ankit] Add. speaker details

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,37 +1,44 @@
 import React from "react";
 
 import "./Speakers.css";
-import speakerImg from "../assets/speaker-img.jpeg";
+import { SpeakerDetails } from "../helpers/SpeakerDetails";
 
-const SpeakerDescription = () => {
+const SpeakerDescription = ({ name, designation, image}) => {
+
   return (
     <div className="speakers_list_description">
       <div className="image">
-        <img src={speakerImg} alt="speaker-img" className="speakers_image" />
+        <img src={image} alt="speaker-img" className="speakers_image" />
       </div>
 
       <div className="speaker_details">
-        <p className="speaker_name">Bharat Krishnamurthy</p>
-        <p className="speaker_designation">Vice President</p>
-        <p className="speaker_company">CredAvenue</p>
+        <p className="speaker_name"> {name} </p>
+        <p className="speaker_designation"> {designation} </p>
+        {/* <p className="speaker_company">CredAvenue</p> */}
       </div>
     </div>
   );
 };
 
 export const Speakers = () => {
+
   return (
     <div className="speakers_section" id="speakers">
       <p className="speakers_heading">Speakers</p>
       <div className="speaker_list">
-        <SpeakerDescription />
-        <SpeakerDescription />
-        <SpeakerDescription />
-        <SpeakerDescription />
-        <SpeakerDescription />
-        <SpeakerDescription />
-        <SpeakerDescription />
-        <SpeakerDescription />
+        {
+          SpeakerDetails.map((speaker, index) => {
+            console.log(speaker);
+
+            return (
+              <SpeakerDescription 
+                key={index}
+                name={speaker.name} 
+                designation={speaker.designation} 
+                image={speaker.image} />
+            );
+          })
+        }
       </div>
     </div>
   );
---
+++ src/helpers/SpeakerDetails.js
@@ -0,0 +1,93 @@
+import Aditya_Ramkumar from './../assets/SpeakerImages/Adityan_Ramkumar.jpg';
+import Ajit_Dongre from './../assets/SpeakerImages/Ajit_Dongre.jpg';
+import Aman_Tiwari from './../assets/SpeakerImages/Aman_Tiwari.png';
+import Anumeha_Verma from './../assets/SpeakerImages/Anumeha.jpeg';
+import Ashwin_Sugavanam from './../assets/SpeakerImages/Ashwin_Sugavanam.jpg';
+import Balvinder_Khurana from './../assets/SpeakerImages/Balvinder_Khurana.jpg';
+import Bharath_Hemachandran from './../assets/SpeakerImages/Bharath_Hemachandran.png';
+import Inderpreet_Singh from './../assets/SpeakerImages/Inderpreet_Singh.jpg';
+import Jeremy_Jackson from './../assets/SpeakerImages/Jeremy_Jackson.jpg';
+import Mageswaran_Muthukumar from './../assets/SpeakerImages/Mageswaran_Muthukumar.jpg';
+import Manisha_Jain from './../assets/SpeakerImages/Manisha_Jain.png';
+import Murali_Poola from './../assets/SpeakerImages/MuraliPoola.jpg';
+import Paritosh_Gupta from './../assets/SpeakerImages/Paritosh_Gupta.png';
+import Prathamesh from './../assets/SpeakerImages/Prathamesh.jpg';
+import Sarang_Shinde from'./../assets/SpeakerImages/sarang_shinde.jpg';
+
+export const SpeakerDetails = [
+    {
+        name: 'Adityan Ramkumar',
+        designation: 'Global Head Of Talent Analytics',
+        image: Aditya_Ramkumar
+    },
+    {
+        name: 'Ajit Dongre',
+        designation: 'Lead Data Engineer',
+        image: Ajit_Dongre
+    },
+    {
+        name: 'Aman Tiwari',
+        designation: 'Data Engineer',
+        image: Aman_Tiwari
+    },
+    {
+        name: 'Anumeha Verma',
+        designation: 'Principal Business Analyst',
+        image: Anumeha_Verma
+    },
+    {
+        name: 'Ashwin Sugavanam',
+        designation: 'Principal Solution Architect',
+        image: Ashwin_Sugavanam
+    },
+    {
+        name: 'Balvinder Khurana',
+        designation: 'Principal Data Engineer',
+        image:  Balvinder_Khurana
+    },
+    {
+        name: 'Bharath Hemachandran',
+        designation: 'Principal Quality Analyst',
+        image: Bharath_Hemachandran
+    },
+    {
+        name: 'Inderpreet Singh',
+        designation: 'Principal Domain Specialist',
+        image: Inderpreet_Singh
+    },
+    {
+        name: 'Jeremy Jackson',
+        designation: 'Senior Data Scientist',
+        image: Jeremy_Jackson
+    },
+    {
+        name: 'Mageswaran Muthukumar',
+        designation: 'Lead Developer',
+        image: Mageswaran_Muthukumar
+    },
+    {
+        name: 'Manisha Jain',
+        designation: 'Lead Data Engineer',
+        image: Manisha_Jain
+    },
+    {
+        name: 'Murali Poola',
+        designation: 'Principal Developer',
+        image: Murali_Poola
+    },
+    {
+        name: 'Paritosh Gupta',
+        designation: 'Senior Data Scientist',
+        image: Paritosh_Gupta
+    },
+    {
+        name: 'Prathamesh Kalamkar',
+        designation: 'Lead Data Scientist',
+        image: Prathamesh
+    },
+    {
+        name: 'Sarang Shinde',
+        designation: 'Lead Data Engineer',
+        image: Sarang_Shinde
+    }
+];
